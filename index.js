4f15ab022751f4d6f640f78e68f11aa0ff83e502
Allison <amh03160@gmail.com>
2022-02-22 11:52:09 -0600

    commit tree dcf15430bf53c35d0a6e128008426f429e7ac56b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+//currentUser
+const currentUser = 'Grace Hopper';
+
+//welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//shortGreeting
+const shortGreeting =  `Welcome, ${currentUser.slice(0,1)}!`;
